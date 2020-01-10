e5667eab484373669d4e3735f4987109443a5af3
mark <mark.poling@gmail.com>
2020-01-10 00:43:24 -0500

    Baby steps.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fbstart
 Getting Started with Firebase
+
+Simple framework for Firebase projects using Google App Engine and Go.
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+	}
+}
+
+func run() (e error) {
+	fmt.Println("Hello world!")
+	return // omitting explicit return value; e scoped in function call
+}
+
+func initdb() (e error) {
+	// Use the application default credentials
+
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: "fbstart"}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	defer client.Close()
+	return
 }
