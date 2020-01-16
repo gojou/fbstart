31d29770d49584b94632e0de21be9dcb31dd8f7a
mark <mark.poling@gmail.com>
2020-01-16 03:57:28 -0500

    boogey boogey

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gojou/fbstart/pkg/entities"
+	"github.com/gojou/fbstart/pkg/handlers"
 
 	"cloud.google.com/go/firestore"
 )
@@ -22,13 +24,33 @@ func run() (e error) {
 	scout := entities.New("Poling", "Aden")
 	fmt.Println("Hello world! " + scout.Greeter())
 
-	// initialize the web server.
+	// initialize the web server
+	w = initweb()
+	if e != nil {
+		return
+	}
+
+	// initialize storage, in this case firestore
 	e = initdb()
 	if e != nil {
 		return
 	}
 	e = errors.New("oops i did it again")
 	return // omitting explicit return value; e scoped in function call
+}
+
+func initweb() (e error) {
+	http.HandleFunc("/", handlers.Index)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	return nil
 }
 
 func initdb() (e error) {
---
+++ pkg/handldlers/index.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// TemplateParams is TemplateParams
+type TemplateParams struct {
+	Notice    string
+	ID        int
+	FirstName string
+	LastName  string
+	EmailAddr string
+	Phone     string
+	Message   string
+}
+
+// Index is index.
+func Index(w http.ResponseWriter, r *http.Request) {
+	params := TemplateParams{}
+
+	// Set the default page
+
+	page := template.Must(template.ParseFiles(
+		"static/html/_base.html",
+		"static/html/index.html",
+	))
+
+	//Display 404 if it's odball URL
+	if r.URL.Path != "/" {
+		page = template.Must(template.ParseFiles(
+			"static/html/_base.html",
+			"static/html/404.html",
+		))
+
+	}
+
+	if r.Method == "GET" {
+		page.Execute(w, params)
+		//	return
+	}
+
+}
